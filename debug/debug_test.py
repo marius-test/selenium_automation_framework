bab84a3c89ac6f174e63d04e9e57f0f26b5145c4
Marius Briscut <marius.briscut5@gmail.com>
2025-07-07 16:22:01 -0400

    added custom waits file and more

---
+++ debug/debug_test.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# <========== TEST CASES STARTS HERE ==========>
+
+import unittest
+import requests
+
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+
+from utils.driver_factory import get_driver, quit_driver
+from utils.waits import wait_for_presence
+
+# TEST DATA
+URL = "https://www.saucedemo.com/"
+
+# <========== TEST CASES ENDS HERE ==========>
+
+
+if __name__ == "__main__":
+    print("It works!")
--- tests/_test_template.py
+++ tests/_test_template.py
@@ -1,28 +1,20 @@
-# delete unused imports
-import time
 import unittest
+from time import sleep
+
 import urllib3
 import requests
 import pyautogui
 from pynput.keyboard import Key, Controller
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException
 
-PATH = Service("C:\\Users\\marius\\webdriver\\chromedriver.exe")
-# driver = webdriver.Chrome(service=PATH)
-driver = webdriver.Chrome(ChromeDriverManager().install())
+from utils.driver_factory import get_driver, quit_driver
+from utils import  waits
+from seletools.actions import drag_and_drop
 
-# action_chains = ActionChains(driver)
-# alert = Alert(driver)
-
-url = "https://www.saucedemo.com/"
-
-if __name__ == '__main__':
-    unittest.main()
+# TEST DATA
+URL = "https://the-internet.herokuapp.com"
