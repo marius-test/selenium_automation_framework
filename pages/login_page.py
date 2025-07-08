9d43af1d7447c4316f80278dc4639f524df12580
Marius Briscut <marius.briscut5@gmail.com>
2025-07-08 11:50:42 -0400

    complete login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,13 +6,15 @@ from config import LOGIN_URL
 class LoginPage(BasePage):
     URL = LOGIN_URL
     
+    LOGIN_LOGO = (By.CLASS_NAME, "login_logo")
+    
     USERNAME_INPUT = (By.ID, "user-name")
     PASSWORD_INPUT = (By.ID, "password")
     LOGIN_BUTTON = (By.ID, "login-button")
     
     ERROR_MESSAGE = (By.CSS_SELECTOR, "div.error-message-container.error > h3")
-    
-    
+    ERROR_CLOSE_BUTTON = (By.CSS_SELECTOR, "div.error-message-container.error button")
+
     def login(self, username, password):
         self.type(self.USERNAME_INPUT, username)
         self.type(self.PASSWORD_INPUT, password)
@@ -20,3 +22,21 @@ class LoginPage(BasePage):
 
     def is_error_displayed(self):
         return self.is_visible(self.ERROR_MESSAGE)
+
+    def get_error_message(self):
+        return self.get_text(self.ERROR_MESSAGE)
+
+    def close_error_message(self):
+        self.click(self.ERROR_CLOSE_BUTTON)
+    
+    def login_logo_text(self):
+        return self.get_text(self.LOGIN_LOGO)
+    
+    def get_username_placeholder(self):
+        return self.find(self.USERNAME_INPUT).get_attribute("placeholder")
+
+    def get_password_placeholder(self):
+        return self.find(self.PASSWORD_INPUT).get_attribute("placeholder")
+    
+    def get_login_button_text(self):
+        return self.find(self.LOGIN_BUTTON).get_attribute("value")
