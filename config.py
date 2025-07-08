3ba8acdbf3731184bb431b0693380708d10413fd
Marius Briscut <marius.briscut5@gmail.com>
2025-07-08 18:44:45 -0400

    organized file

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+# ---------------------------
 # headless browser flag
+# ---------------------------
 HEADLESS = False  # set True to run tests headless globally
 
+# ---------------------------
 # urls
+# ---------------------------
 BASE_URL = LOGIN_URL = "https://www.saucedemo.com/"
 INVENTORY_URL = "https://www.saucedemo.com/inventory.html"
 CART_URL = "https://www.saucedemo.com/cart.html"
@@ -9,7 +13,9 @@ CHECKOUT_STEP_ONE_URL = "https://www.saucedemo.com/checkout-step-one.html"
 CHECKOUT_STEP_TWO_URL = "https://www.saucedemo.com/checkout-step-two.html"
 CHECKOUT_COMPLETE_URL = "https://www.saucedemo.com/checkout-complete.html"
 
+# ---------------------------
 # users
+# ---------------------------
 STANDARD_USER = "standard_user"
 LOCKED_OUT_USER = "locked_out_user"
 PROBLEM_USER = "problem_user"
@@ -17,5 +23,7 @@ PERFORMANCE_GLITCH_USER = "performance_glitch_user"
 ERROR_USER = "error_user"
 VISUAL_USER = "visual_user"
 
+# ---------------------------
 # password
+# ---------------------------
 PASSWORD = "secret_sauce"
